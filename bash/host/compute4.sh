b16c3f6be848d3986ad96f437446e942565a6c21
Russ Nelson <rnelson@compute4.datacenter.paneve>
2013-08-22 10:19:39 -0600

    Added environment-setup stuff for compute server

--- bash/host/compute4.sh
+++ bash/host/compute4.sh
@@ -2,3 +2,18 @@
 
 alias ss=/apps/smartsvn/latest/bin/smartsvn.sh
 
+# Source the Sankhya Tools (only do before Forte setup, not after):
+. /apps/scripts/stc.sh
+
+# Source the Forte Tools:
+. /apps/scripts/forte.sh > /dev/null
+
+# Source the Xilinx Tools:
+. /apps/scripts/xilinx.sh > /dev/null
+
+# Coverage setup
+. /apps/scripts/jeda_1_3_3.sh > /dev/null
+
+# Mentor Questa setup
+. /apps/scripts/mentor.sh > /dev/null
+
