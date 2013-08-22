09a76f2f751c86158896a6e45e6141f280e25f9b
Russ Nelson <rnelson@compute4.datacenter.paneve>
2013-08-22 13:52:48 -0600

    Bash arrow-keys needed to be platform-specific

--- bash/os/linux-gnu.sh
+++ bash/os/linux-gnu.sh
@@ -1,4 +1,10 @@
 # bash setting for linux
 
+# Set up ctrl-arrow keys for searching
+bind '"\e[1;5A": history-search-backward'
+bind '"\e[1;5B": history-search-forward'
+bind '"\e[1;5C": forward-word'
+bind '"\e[1;5D": backward-word'
+
 export RUSS_LS_OPTS='-Fh --color=auto'
 
