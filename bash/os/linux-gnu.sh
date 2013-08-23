13deb4fec68694cd1c9cc5046d831ba93b5e8b18
Russ Nelson <rnelson@compute4.datacenter.paneve>
2013-08-22 22:52:14 -0600

    Made bash ctrl-arrow keys work in VNC _and_ SSH

--- bash/os/linux-gnu.sh
+++ bash/os/linux-gnu.sh
@@ -5,6 +5,10 @@ bind '"\e[1;5A": history-search-backward'
 bind '"\e[1;5B": history-search-forward'
 bind '"\e[1;5C": forward-word'
 bind '"\e[1;5D": backward-word'
+bind '"\e[5A": history-search-backward'
+bind '"\e[5B": history-search-forward'
+bind '"\e[5C": forward-word'
+bind '"\e[5D": backward-word'
 
 export RUSS_LS_OPTS='-Fh --color=auto'
 
