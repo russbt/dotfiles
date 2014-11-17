208b9e6bedb74aab35bf80e4c0e59bca3f120dda
Russ <russbt@users.noreply.github.com>
2014-11-17 12:33:53 -0700

    Added tmux configuration to install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ mv -f .bashrc .bashrc.off
 mv -f .bash_profile .bash_profile.off
 mv -f .vim .vim.off
 mv -f .vimrc .vimrc.off
+mv -f .tmux.conf .tmux.conf.off
 
 ln -s dotfiles/bash/bashrc .bashrc
 ln -s dotfiles/bash/bash_profile .bash_profile
 ln -s dotfiles/vim/vimrc.vim .vimrc
 ln -s dotfiles/vim .vim
+ln -s dotfiles/tmux.conf .tmux.conf
 
