931822a58eb99efb8a44c8d987eeceec9c038bf5
Russ <russ@spudbuntu>
2022-01-26 14:13:42 -0700

    Adding host-specific stuff for spudbuntu

---
+++ bash/host/spudbuntu.sh
@@ -0,0 +1,4 @@
+alias hoobs_dn="VBoxManage controlvm 661856e7-0afa-4751-874b-58dde5846bf1 poweroff"
+alias hoobs_up="VBoxManage startvm 661856e7-0afa-4751-874b-58dde5846bf1"
+alias hoobs_stat="VBoxManage list -l runningvms"
+
